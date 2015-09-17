30d3ae45b29f7cf263209548dce325f876004e1b
Cody Russell <cody@jhu.edu>
2015-09-16 23:01:06 -0500

    Bump poison to 1.5.0

--- mix.exs
+++ mix.exs
@@ -26,7 +26,7 @@ defmodule Reaxt.Mixfile do
      {:poolboy, []},
      {:cowboy,"~> 1.0.0"},
      {:plug,"~> 1.0"},
-     {:poison,"~> 1.4.0"}]
+     {:poison,"~> 1.5.0"}]
   end
 
   defp package do
